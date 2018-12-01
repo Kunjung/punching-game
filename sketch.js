9ff21d9c1171a11d7f63f87450f7605a399604d0
Kunjung Sherpa <kunjungsherpa@gmail.com>
2018-12-01 10:44:13 +0545

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(200);
+}
+
+function draw() {
+	rectMode(CENTER);
+	fill(255, 0, 0);
+	noStroke();
+	rect(width/2, height-40, 40, 40);
+
+}
